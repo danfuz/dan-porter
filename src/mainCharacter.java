36b5f3f2585dd34f2bc19a970b69242262585398
danfuz <danryafuz@gmail.com>
2019-09-03 10:11:04 -0400

    jljho

--- src/mainCharacter.java
+++ src/mainCharacter.java
@@ -1,3 +1,11 @@
 public class mainCharacter {
-    
+    private int xcenter, ycenter, width, height, x,y;
+    public mainCharacter(int xx, int yy, int w, int h){
+        x = xx;
+        y = yy;
+        width = w;
+        height = h;
+        xcenter = x+width/2;
+        ycenter = y+height/2;
+    }
 }
