6f4ad2ea25d962b6faece749d2eff1df7cb75d9f
porter_moody <porter_moody@student.wayland.k12.ma.us>
2019-09-11 12:27:26 -0400

    ss

--- src/Enemies.java
+++ src/Enemies.java
@@ -45,4 +45,19 @@ public class Enemies {
         }
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 }
