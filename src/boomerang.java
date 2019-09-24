c3a82c7786e96fd75130843358d9309433fbafbf
porter_moody <porter_moody@student.wayland.k12.ma.us>
2019-09-24 13:33:07 -0400

    ss

--- src/boomerang.java
+++ src/boomerang.java
@@ -54,7 +54,23 @@ public class boomerang {
     }
 
 
-//    public void move(int tx, int ty){
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    //    public void move(int tx, int ty){
 //
 //        if (!call) {
 //            int dist = 0;
